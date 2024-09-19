15f37d27b857d7ed0eaa849c04e5285eed6f28e6
Miguel <148611495+RyMigzz@users.noreply.github.com>
2024-09-19 07:10:23 -0700

    Add files via upload

---
+++ SOFTDEV3FINALNAJUD/index.php
@@ -0,0 +1,17 @@
+<?php include('includes/navbar.php');?>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="includes/style.css">
+<div class="py-5">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>CebuTech Bistro</h1>
+                <a href="login.php" class="btn btn-primary mt-4">Login</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <?php include ('includes/footer.php');?>
